b9ce70d72092418ec856c484385a53a0677fc153
bala-amypo <bala@amypo.in>
2025-12-17 06:03:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,5 +5,16 @@ import org.springframework.web.bind.annotations.*;
 @RestController
 public class StudentController{
 
-    @Auto
+    @AutoWired
+    StudentService ser;
+
+    @PostMapping("/adddata")
+    public Student createData(@RequestBody Student stu){
+        return ser.createDate(stu);
+    }
+
+    @GetMapping("/fetchdata")
+    public List<Student> fetchRecord(){
+        return ser.fetchRecord();
+    }
 }
