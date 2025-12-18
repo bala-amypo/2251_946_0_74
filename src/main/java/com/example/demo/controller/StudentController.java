98962966044a7484c57022a8131a87693bb07a96
bala-amypo <bala@amypo.in>
2025-12-18 07:09:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -33,8 +33,8 @@ public class StudentController{
     public String updateById(@PathVariable int id,@RequestBody Student stu){
         Optional<Student> student = ser.fetchById(id);
         if(student.isPresent()){
-            return stu.setId(id);
-            return ser.createDate(stu);
+            stu.setId(id);
+            ser.createData(stu);
             return "Data updated successfully";
         }else{
             return id + "Not found";
